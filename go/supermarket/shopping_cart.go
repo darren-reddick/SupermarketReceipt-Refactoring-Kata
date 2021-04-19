c214776efad12b0f07c0029b491281210374a6b9
dreddick.home2@gmail.com <dreddick.home2@gmail.com>
2021-04-19 09:58:24 +0100

    remove addItem function

--- go/supermarket/shopping_cart.go
+++ go/supermarket/shopping_cart.go
@@ -6,12 +6,12 @@ import (
 )
 
 type ProductQuantity struct {
-	product Product
+	product  Product
 	quantity float64
 }
 
 type ShoppingCart struct {
-	items []ProductQuantity
+	items             []ProductQuantity
 	productQuantities map[Product]float64
 }
 
@@ -20,10 +20,6 @@ func NewShoppingCart() *ShoppingCart {
 	s.items = []ProductQuantity{}
 	s.productQuantities = make(map[Product]float64)
 	return &s
-}
-
-func (c *ShoppingCart) addItem(product Product) {
-	c.addItemQuantity(product, 1)
 }
 
 func (c *ShoppingCart) addItemQuantity(product Product, amount float64) {
@@ -39,7 +35,7 @@ func (c *ShoppingCart) addItemQuantity(product Product, amount float64) {
 func (c *ShoppingCart) handleOffers(receipt *Receipt, offers map[Product]SpecialOffer, catalog Catalog) {
 	for p, _ := range c.productQuantities {
 		var quantity = c.productQuantities[p]
-		if offer, ok := offers[p]; ok  {
+		if offer, ok := offers[p]; ok {
 			var unitPrice = catalog.unitPrice(p)
 			var quantityAsInt = int(math.Round(quantity))
 			var discount *Discount = nil
@@ -50,8 +46,8 @@ func (c *ShoppingCart) handleOffers(receipt *Receipt, offers map[Product]Special
 			} else if offer.offerType == TwoForAmount {
 				x = 2
 				if quantityAsInt >= 2 {
-					var total = offer.argument * float64(quantityAsInt / x) + float64(quantityAsInt % 2) * unitPrice
-					var discountN = unitPrice * quantity - total;
+					var total = offer.argument*float64(quantityAsInt/x) + float64(quantityAsInt%2)*unitPrice
+					var discountN = unitPrice*quantity - total
 					discount = &Discount{product: p, description: fmt.Sprintf("2 for %.2f", offer.argument), discountAmount: -discountN}
 				}
 
@@ -59,16 +55,16 @@ func (c *ShoppingCart) handleOffers(receipt *Receipt, offers map[Product]Special
 			if offer.offerType == FiveForAmount {
 				x = 5
 			}
-			var numberOfXs int = quantityAsInt / x;
+			var numberOfXs int = quantityAsInt / x
 			if offer.offerType == ThreeForTwo && quantityAsInt > 2 {
-				var discountAmount = quantity * unitPrice - (float64(numberOfXs * 2) * unitPrice + float64(quantityAsInt % 3) * unitPrice)
+				var discountAmount = quantity*unitPrice - (float64(numberOfXs*2)*unitPrice + float64(quantityAsInt%3)*unitPrice)
 				discount = &Discount{product: p, description: "3 for 2", discountAmount: -discountAmount}
 			}
 			if offer.offerType == TenPercentDiscount {
 				discount = &Discount{product: p, description: fmt.Sprintf("%.0f %% off", offer.argument), discountAmount: -quantity * unitPrice * offer.argument / 100.0}
 			}
 			if offer.offerType == FiveForAmount && quantityAsInt >= 5 {
-				var discountTotal = unitPrice * quantity - (offer.argument * float64(numberOfXs) + float64(quantityAsInt % 5) * unitPrice)
+				var discountTotal = unitPrice*quantity - (offer.argument*float64(numberOfXs) + float64(quantityAsInt%5)*unitPrice)
 				discount = &Discount{product: p, description: fmt.Sprintf("%d for %.2f", x, offer.argument), discountAmount: -discountTotal}
 			}
 			if discount != nil {
